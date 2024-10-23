614cc0a9a8465b3097aca9567467f223b5bbbe9a
ANANDA BHOI <anandabhoi5582@gmail.com>
2024-10-23 20:41:06 +0530

    3nd commit

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -7,22 +7,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 
-            val flStartButton: FrameLayout = findViewById(R.id.flstart)
-            flStartButton.setOnClickListener {
+            //val flStartButton: FrameLayout = findViewById(R.id.flstart)
+            binding?.flstart?.setOnClickListener {
                 Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+            }
+            fun onDestroy() {
+                super.onDestroy()
+                binding = null
             }
 
             insets
         }
+
     }
 }
