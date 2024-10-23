17a76874c870d61c0313ad81f4bf90cf3e16029f
ANANDA BHOI <anandabhoi5582@gmail.com>
2024-10-23 20:24:59 +0530

    3nd commit

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication1
 
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+            val flStartButton: FrameLayout = findViewById(R.id.flstart)
+            flStartButton.setOnClickListener {
+                Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+            }
+
             insets
         }
     }
