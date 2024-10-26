7a439cb8b7817cfdfda0f58120827bd134c34eb7
ANANDA BHOI <anandabhoi5582@gmail.com>
2024-10-26 16:47:48 +0530

    5th commit

--- app/src/main/java/com/example/myapplication1/ExerciseActivity.kt
+++ app/src/main/java/com/example/myapplication1/ExerciseActivity.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication1
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.speech.tts.TextToSpeech
@@ -25,6 +27,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var currentExercisePosition=-1
 
     private var tts: TextToSpeech?=null
+    private var player: MediaPlayer?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,6 +58,15 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         }
     }
     private fun setupRestView(){
+
+        try{
+            val soundURI = Uri.parse("android.resource://com.example.myapplication1/" + R.raw.press_start)
+            player=MediaPlayer.create(applicationContext,soundURI)
+            player?.isLooping=false
+            player?.start()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
 
         binding?.flRestView?.visibility= View.VISIBLE
         binding?.tvTitle?.visibility=View.VISIBLE
@@ -153,6 +165,10 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         if(tts!=null){
             tts!!.stop()
             tts!!.shutdown()
+
+        }
+        if(player!=null){
+            player!!.stop()
         }
 
         binding=null
